1ce123b6db5c444439d733a97f9a39435f449a5f
AlexMinsk <asdrubael@tut.by>
2017-08-29 17:26:07 +0300

    add 18

---
+++ 18.php
@@ -0,0 +1,32 @@
+<?php
+//диапозон от N до M
+$N = 1;
+$M = 10000;
+$sumFirst = 0;//сумма делителей первого числа
+$sumSecond = 0;//сумма делителей второго числа
+
+for($i = $N; $i <= $M; $i++){
+
+    for($x = 1; $x < $i; $x++){//сложить все делители первого числа
+        $res = $i / $x;
+
+        if(gettype($res) == "integer") {
+            $sumFirst += $x;
+        }
+    }
+
+    for($y = 1; $y < $sumFirst; $y++){//сложить все делители второго числа
+        $res = $sumFirst / $y;
+
+        if(gettype($res) == "integer") {
+            $sumSecond += $y;
+        }
+    }
+
+    if($i == $sumSecond && $i != $sumFirst){
+        echo '[ ', $i, ' & ', $sumFirst,  ' ]';
+    }
+
+    $sumFirst = 0;
+    $sumSecond = 0;
+}
