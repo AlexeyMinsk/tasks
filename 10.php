7a68a815e9ebb5d94e7398d126257a82cef4d0a7
AlexMinsk <asdrubael@tut.by>
2017-08-29 14:28:26 +0300

    add 10 task

---
+++ 10.php
@@ -0,0 +1,54 @@
+<?php
+function getExp($num){
+
+    $counter = 0;
+    $i = 1;
+
+    if($num < 10)
+        return 0;
+
+    while($num > $i){
+
+        $i *= 10;
+        $counter++;
+    }
+
+    return $counter - 1;
+}
+
+function getNum($num, $pos){//получить число по порядковому номеру
+
+    $pos = getExp($num) - $pos;
+    return ( (int)($num / pow(10, $pos)) ) % 10;
+
+}
+
+function testPalindrom($num){
+
+    $sz = getExp($num);
+
+    if(!$sz)
+        return false;
+
+    for($i = 0; $i < $sz / 2; $i++){
+
+        $num1 = getNum($num, $i);
+        $num2 = getNum($num, $sz - $i);
+
+        if($num1 != $num2){
+            return false;
+        }
+    }
+
+    return true;
+}
+$N = 1000;//искать до
+
+for($i = 10; $i < $N; $i++){
+    if( testPalindrom($i) ){
+        echo $i;
+        echo '|';
+    }
+}
+
+
