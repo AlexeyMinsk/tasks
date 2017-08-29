9b7531de90a1fce771de5adf3f7e43c12fb81cbb
AlexMinsk <asdrubael@tut.by>
2017-08-29 10:43:15 +0300

    fix 3 task

--- 3.php
+++ 3.php
@@ -1,27 +1,27 @@
 <?php
-$num = 12345;//заданное число
+$num = 2234;//заданное число
 $flag = true;
 $preResidue = 9;
 
-while(true){
-	
-	$residue = $num % 10;
-	
-	if($preResidue < $residue){
-		$flag = false;
-		break;
-	}
-	
-	$preResidue = $residue;
-	
-	$num = ($num - $residue) / 10;	
-	
-	if($num < 10)
-		break;
+while($num > 9){
+
+    $residue = $num % 10;
+
+    if($preResidue <= $residue){
+        $flag = false;
+        break;
+    }
+
+    $preResidue = $residue;
+
+    $num = ($num - $residue) / 10;
 }
 
+if($flag && $preResidue <= $num)
+    $flag = false;
+
 if($flag)
-	echo "Образуют";
+    echo "Образуют";
 else
-	echo "Не образуют";
+    echo "Не образуют";
 ?>
