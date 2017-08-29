5ed8849b65e7be3d292d0261977764438a0b5e98
AlexMinsk <asdrubael@tut.by>
2017-08-29 11:04:45 +0300

    4 task fix

--- 4.php
+++ 4.php
@@ -1,63 +1,46 @@
 <?php
-function order($num, $fl = false){
-//fl - порядок, true - восходящий
-	
-$flag = true;
-$preResidue = $num % 10;
-$value = (int)($num / 1000);
+function order($num, $ord = true){
+    //$ord - порядок, true - на возрастание
+    $flag = true;
+    $preResidue = $ord? 9 : 0;
 
-while(true){
-	
-	$residue = $num % 10;
-	
-	if($residue == 0){
-		$flag = false;
-		break;
-	}
-	
-	if($fl && $residue < $value){
-		$flag = false;
-		break;
-	}elseif(!$fl && $residue > $value){
-		$flag = false;
-		break;
-	}
-	
-	if($fl){
-		if($preResidue < $residue){
-			$flag = false;
-			break;
-		}
-	}else{
-		if($preResidue > $residue){
-			$flag = false;
-			break;
-		}
-	}
-	
-	$num = ($num - $residue) / 10;	
-	
-	if($num < 10)	break;
-	
-	$preResidue = $residue;
-}
+    while($num > 9){
 
-if($flag)
-	return true;
-else
-	return false;
+        $residue = $num % 10;
+        if($ord) {
+            if ($preResidue <= $residue) {
+                $flag = false;
+                break;
+            }
+        }else{
+            if ($preResidue >= $residue) {
+                $flag = false;
+                break;
+            }
+        }
+        $preResidue = $residue;
+
+        $num = ($num - $residue) / 10;
+    }
+    if($ord) {
+        if ($flag && $preResidue <= $num)
+            $flag = false;
+    }else{
+        if ($flag && $preResidue >= $num)
+            $flag = false;
+    }
+    return $flag;
 }
 
 for($i = 1001; $i < 10000;$i++){
-	
-	if( !($i%2) ){
-		if( order($i, true) ){
-			echo $i;
-			echo '|->';
-		}elseif( order($i) ){
-			echo $i;
-			echo '|<-';
-		}
-	}
+
+    if( !($i%2) ){
+        if( order($i) ){
+            echo '|->';
+            echo $i;
+        }elseif( order($i, false) ){
+            echo '|<-';
+            echo $i;
+        }
+    }
 }
-?>
