070db12d7004cfd35fc96b74143cc0d5a715fb93
AlexMinsk <asdrubael@tut.by>
2017-08-30 17:09:10 +0300

    fix 1

--- 1.php
+++ 1.php
@@ -2,15 +2,13 @@
 $N = 133468;
 $counter = 0;
 
-while($N > 9){
+while($N){
     $num = $N % 10;
     $N = ($N - $num) / 10;
 
     if($num < 5)
         $counter++;
 }
-if($N < 5)
-    $counter++;
 
 echo $counter;
 ?>
