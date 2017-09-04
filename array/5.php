2f6be9db509b8cb479e4ee435856c3bc5bfd8763
AlexMinsk <asdrubael@tut.by>
2017-09-04 12:30:31 +0300

    add 5 task

---
+++ array/5.php
@@ -0,0 +1,27 @@
+<?php
+$arrA = array(1, 55, 15, 77, 54, -1, 66);
+$i = 0;
+$indexMax = 0;
+$indexMin = 0;
+$min = $arrA[0];
+$max = $arrA[0];
+
+while( $i < sizeof($arrA) ){
+
+    if( $arrA[$i] < $min){
+        $indexMin = $i;
+        $min = $arrA[$i];
+    }
+    if( $arrA[$i] > $max){
+        $indexMax = $i;
+        $max = $arrA[$i];
+    }
+    ++$i;
+}
+
+$temp = $arrA[$indexMax];
+$arrA[$indexMax] = $arrA[$indexMin];
+$arrA[$indexMin] = $temp;
+
+print_r($arrA);
+
