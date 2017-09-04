c8674ca0d6fb77ee481f538c6aac9930696c02c7
AlexMinsk <asdrubael@tut.by>
2017-09-04 15:37:11 +0300

    add 10

---
+++ array/10.php
@@ -0,0 +1,53 @@
+<?php
+function getMaxElement(array $arrA){
+
+    if(!sizeof($arrA))
+        die("Массив пуст");
+
+    $i = 0;
+    $max = $arrA[0];
+
+    while( $i < sizeof($arrA) ){
+
+        if( $arrA[$i] > $max && !($arrA[$i]%2) ) {
+            $indexMax = $i;
+            $max = $arrA[$i];
+        }
+        ++$i;
+    }
+    return [$indexMax, $max];
+}
+
+function getMinElement(array $arrA){
+
+    if(!sizeof($arrA))
+        die("Массив пуст");
+
+    $i = 0;
+    $min = $arrA[0];
+
+    while( $i < sizeof($arrA) ){
+
+        if( $arrA[$i] < $min){
+            $indexMin = $i;
+            $min = $arrA[$i];
+        }
+        ++$i;
+    }
+    return [$indexMin, $min];
+}
+
+$arrayNum = array(1, -33, 4, 444, 789, 22);
+
+$max = getMaxElement($arrayNum);
+$min = getMinElement($arrayNum);
+
+for($i = 0; $i < count($arrayNum); $i++){
+
+    if($arrayNum[$i] % 2)
+        $arrayNum[$i] = $min[1];
+    else
+        $arrayNum[$i] = $max[1];
+}
+
+print_r($arrayNum);
