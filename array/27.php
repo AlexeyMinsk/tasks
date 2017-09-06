100ba8eebcc8760233b8d3c22bb41f4c4a59d51a
AlexMinsk <asdrubael@tut.by>
2017-09-06 11:35:20 +0300

    add 27

--- array/27.php
+++ array/27.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 06.09.2017
- * Time: 10:20
- */
+function insertElem(array &$arr, $elem){
+
+    $adj = 0;
+    $fl = true;
+    $arrB = array();
+    $size = count($arr);
+
+    for($i = 0; $i < $size; $i++){
+
+        if($arr[$i] > $elem && $fl){
+            $arrB[] = $elem;
+            $fl = false;
+        }
+        $arrB[] = $arr[$i];
+
+        if($fl && $size == ($i+1) )
+            $arrB[] = $elem;
+    }
+    $arr = $arrB;
+}
+
+$arrA = array(1, 5, 6, 8);
+
+insertElem($arrA, 7);
+insertElem($arrA, 55);
+insertElem($arrA, -7);
+insertElem($arrA, 0);
+
+print_r($arrA);
+
+
