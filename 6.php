4903be80972975649b0cdc8e98c573c4cc86b003
AlexMinsk <asdrubael@tut.by>
2017-09-07 16:21:10 +0300

    fix 6

--- 6.php
+++ 6.php
@@ -1,4 +1,19 @@
 <?php
+function unique($num){//проверка на уникальность
+
+    for ($n = 0; $n < 4; $n++) {
+
+        $a = $num / pow(10, $n) % 10;
+
+        for ($m = $n+1; $m < 4; $m++) {
+            $b = $num / pow(10, $m) % 10;
+            if($a == $b)
+                return false;
+        }
+    }
+
+    return true;
+}
 //заданнные по условиям числа
 $q = 0;
 $w = 2;
@@ -8,14 +23,16 @@ $count = 0;
 
 for($i = 1000; $i < 10000; $i++) {
 
-    for($n = 0; $n < 4; $n++){
+    if(unique($i)) {
+        for ($n = 0; $n < 4; $n++) {
 
-        $a = $i / pow(10, $n) % 10;
-        if($a == $q || $a == $w || $a == $e || $a == $r)
-            $count++;
-        else
-            break;
+            $a = $i / pow(10, $n) % 10;
+            if ($a == $q || $a == $w || $a == $e || $a == $r)
+                $count++;
+            else
+                break;
 
+        }
     }
 
     if($count == 4)
